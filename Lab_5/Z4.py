5650920553dd1a8b43fdd9b9e7bcb100001a2074
COPOKA59 <133018391+COPOKA59@users.noreply.github.com>
2023-11-15 21:16:02 +0500

    5.2
    Изменена кодировка windows-1251

--- Lab_5/Z4.py
+++ Lab_5/Z4.py
@@ -9,15 +9,15 @@ def Z4():
     df = pd.read_csv('Lab_5/marks.csv', encoding = 'windows-1251', sep = ';')
 
     # 2
-    print(f'Вся таблица:\n{df.to_string()}\n')
+    print(f'Р’СЃСЏ С‚Р°Р±Р»РёС†Р°:\n{df.to_string()}\n')
 
     # 3
-    print(f'Первые 50 учеников:\n{df.head(50)}\n')
+    print(f'РџРµСЂРІС‹Рµ 50 СѓС‡РµРЅРёРєРѕРІ:\n{df.head(50)}\n')
 
     # 4
     url = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B3%D0%BE%D1%81%D1%83%D0%B4%D0%B0%D1%80%D1%81%D1%82%D0%B2_%D0%B8_%D0%B7%D0%B0%D0%B2%D0%B8%D1%81%D0%B8%D0%BC%D1%8B%D1%85_%D1%82%D0%B5%D1%80%D1%80%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%B9_%D0%95%D0%B2%D1%80%D0%BE%D0%BF%D1%8B"
-    table = pd.read_html(url, match='(Республика Беларусь)')[0]
-    print(f"{table[['Название (Официальное название)', 'Столица']]}\n")
+    table = pd.read_html(url, match='(Р РµСЃРїСѓР±Р»РёРєР° Р‘РµР»Р°СЂСѓСЃСЊ)')[0]
+    print(f"{table[['РќР°Р·РІР°РЅРёРµ (РћС„РёС†РёР°Р»СЊРЅРѕРµ РЅР°Р·РІР°РЅРёРµ)', 'РЎС‚РѕР»РёС†Р°']]}\n")
 
     # 5
-    print(f'Количество строк: {len(table)}, количество столбцов: {len(table.columns)}.')
+    print(f'РљРѕР»РёС‡РµСЃС‚РІРѕ СЃС‚СЂРѕРє: {len(table)}, РєРѕР»РёС‡РµСЃС‚РІРѕ СЃС‚РѕР»Р±С†РѕРІ: {len(table.columns)}.')
