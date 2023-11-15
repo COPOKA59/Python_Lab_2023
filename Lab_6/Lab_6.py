247d175b60b005906114c2b84ab938e99275f9c5
COPOKA59 <133018391+COPOKA59@users.noreply.github.com>
2023-11-15 21:16:45 +0500

    Lab_6

---
+++ Lab_6/Lab_6.py
@@ -0,0 +1,26 @@
+from Z1 import *
+from Z2 import *
+from Z3 import *
+from Z4 import *
+from Z5 import *
+
+if __name__ == '__main__':
+    run = True
+    while run:
+        print('Выберите номер задания от 1 до 5: ')
+        Z = int(input())
+        if Z == 1:
+            Z1()
+        elif Z == 2:
+            Z2()
+        elif Z == 3:
+            Z3()
+        elif Z == 4:
+            Z4()
+        elif Z == 5:
+            Z5()
+        else:
+            print('Вы ввели неверное число')
+        print('Нажмите любую цифру > 1, что бы продолжить:\nОтменить повтор 1.')
+        if int(input()) == 1:
+            run = False
